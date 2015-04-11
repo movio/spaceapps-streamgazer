d983376e8ac3f55b1e2a73807c997e7c1668ebcf
Isaac Johnston <isaac@movio.co>
2015-04-12 09:46:06 +1200

    feat(web): implement example of a d3 map layer

--- web/resources/public/js/visualization.js
+++ web/resources/public/js/visualization.js
@@ -4,5 +4,37 @@ var map = L.map("map").setView([37.09024, -95.712891], 5);
 
 L.tileLayer("http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
     attribution: "Map data &copy; [...]",
-    maxZoom: 7
+    maxZoom: 13
 }).addTo(map);
+
+var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+    g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+d3.json("/json/us-states.json", function (collection) {
+    var transform = d3.geo.transform({ point: projectPoint }),
+        path = d3.geo.path().projection(transform);
+
+    var feature = g.selectAll("path").data(collection.features).enter().append("path");
+
+    map.on("viewreset", reset);
+    reset();
+
+    // Reposition the SVG to cover the features.
+    function reset() {
+        var bounds = path.bounds(collection),
+            topLeft = bounds[0],
+            bottomRight = bounds[1];
+
+        svg.attr("width", bottomRight[0] - topLeft[0]).attr("height", bottomRight[1] - topLeft[1]).style("left", topLeft[0] + "px").style("top", topLeft[1] + "px");
+
+        g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+        feature.attr("d", path);
+    }
+
+    // Use Leaflet to implement a D3 geometric transformation.
+    function projectPoint(x, y) {
+        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+    }
+});
